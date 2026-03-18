10e14074e1e6dc4dba9fb7e05aef2a1e16bc366c
Kacper Wieczorkowski <s36713@pjwstk.edu.pl>
2026-03-18 11:20:46 +0100

    init

---
+++ src/Ksiazka.java
@@ -0,0 +1,25 @@
+public class Ksiazka {
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
+
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.liczbaStron = liczbaStron;
+        this.dostepna = dostepna;
+
+
+    }
+    public void wypiszInfo() {
+        System.out.println("Tytul:" + this.tytul);
+        System.out.println("Autor:" + this.autor);
+        System.out.println("Liczba stron:" + this.liczbaStron);
+        System.out.println("Dostepna:" + this.dostepna);
+
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Ksiazka Dzuma = new Ksiazka("Dzuma", "Albert Camus", 312, true);
+
+    }
+}
